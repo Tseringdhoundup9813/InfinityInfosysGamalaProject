f0495d95ae7d1514f30b27624221d74fd11fb791
TseringDhoundup1997 <47768382+Tseringdhoundup9813@users.noreply.github.com>
2023-05-07 02:58:59 +0545

    Updates

---
+++ precache-manifest.95962db6b617adff622ea9961df55b3c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "1490a7a250696e228ab3",
+    "url": "/InfinityInfosysGamalaProject/static/css/main.663a96c5.chunk.css"
+  },
+  {
+    "revision": "1490a7a250696e228ab3",
+    "url": "/InfinityInfosysGamalaProject/static/js/main.1490a7a2.chunk.js"
+  },
+  {
+    "revision": "f4a2a6c05474266edc2e",
+    "url": "/InfinityInfosysGamalaProject/static/js/1.f4a2a6c0.chunk.js"
+  },
+  {
+    "revision": "71f3f04d6e4f2da5f627",
+    "url": "/InfinityInfosysGamalaProject/static/js/2.71f3f04d.chunk.js"
+  },
+  {
+    "revision": "e185aca95d6d7f24c45e",
+    "url": "/InfinityInfosysGamalaProject/static/js/runtime~main.e185aca9.js"
+  },
+  {
+    "revision": "b68926250679b64f0a579943699ee9df",
+    "url": "/InfinityInfosysGamalaProject/static/media/cardSound.b6892625.mp3"
+  },
+  {
+    "revision": "5edcedf1a79afed7da4622272159308c",
+    "url": "/InfinityInfosysGamalaProject/index.html"
+  }
+];
